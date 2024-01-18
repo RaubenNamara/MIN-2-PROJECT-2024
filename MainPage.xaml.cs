73c88c91ae81597bc4f570ecd6dab05dd7ffcb3f
Namara Rauben <82104948+RaubenNamara@users.noreply.github.com>
2024-01-18 23:51:27 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+
+namespace GpsGeolocator0._1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public async void GetLocation()
+        {
+            Location Location = await Geolocation.GetLastKnownLocationAsync();
+
+            if (Location == null)
+            {
+                Location = await Geolocation.GetLocationAsync(new GeolocationRequest
+                {
+                    DesiredAccuracy = GeolocationAccuracy.Medium,
+                    Timeout = TimeSpan.FromSeconds(30)
+                });
+            }
+            var placemarks = await Geocoding.GetPlacemarksAsync(Location.Latitude, Location.Longitude);
+            var placemark = placemarks?.FirstOrDefault();
+
+
+            var geocodeAddress =
+
+                $"Country :         {placemark.CountryName}\n" +
+                $"CountryCode :     {placemark.CountryCode}\n" +
+                $"AdminArea :       {placemark.AdminArea}\n" +
+                $"Feature Name:     {placemark.FeatureName}\n" +
+                $"Locality :        {placemark.Locality}\n" +
+                $"SubAdminArea :    {placemark.SubAdminArea}\n" +
+                $"SubLocality :     {placemark.SubLocality}\n" +
+                $"Latitude :        {placemark.Location.Latitude.ToString()}\n" +
+                $"Longitude :       {placemark.Location.Longitude.ToString()}\n" +
+                $"SubThoroughfare:  {placemark.SubThoroughfare}\n" +
+                $"Thoroughfare:     {placemark.Thoroughfare}\n";
+
+            txtLoc.Text = geocodeAddress;
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            GetLocation();
+        }
+
+    }
+}
